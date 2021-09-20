166907b9c2422f78b7eeb05373d26251977a5b72
ilwel <ilwelisaac@hotmail.com>
2021-09-20 16:55:13 -0300

    heroku database

--- db.js
+++ db.js
@@ -1,19 +1,33 @@
 const { Pool } = require('pg')
+// const pool = new Pool({
+
+//     user:'postgres',
+//     host:'localhost',
+//     database:'market_cubos',
+//     password:'viana1920',
+//     port: 5432
+
+// });
+
+//heroku config
 const pool = new Pool({
 
-    user:'postgres',
-    host:'localhost',
-    database:'market_cubos',
-    password:'viana1920',
-    port: 5432
+  user: 'rhhsgcwvllivtc',
+  host: 'ec2-44-195-16-34.compute-1.amazonaws.com',
+  database: 'dfq25tim3ja2gl',
+  password: '7f8bc9910ae38d8b4e8b59dd156c92502c507f2149ca25126c34e345ed6eb0a8',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 
-});
+})
 
 const query = (text, param) => pool.query(text, param);
 
 module.exports = {
 
-    query
+  query
 
 }
 
--- index.js
+++ index.js
@@ -7,8 +7,4 @@ app.use(express.json());
 app.use(router);
 
 const PORT = 8000;
-app.listen(PORT, () => {
-
-    console.log('the server is open on port ' + PORT);
-
-})
+app.listen(process.env.PORT || PORT)
